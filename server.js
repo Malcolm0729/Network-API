4f0fa957bdf87a89c9d789fee2f22ea61b9fa960
Malcolm Hendrix <melanna_camacho@Melannas-MacBook-Pro.local>
2022-04-11 18:58:19 -0700

    half way through server.js and added the port

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const routes = require('./routes')
+const db = require('./config/connection');
+
+const app = express();
+const PORT: 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(routes);
+
+
