fc0d9db8b5f75beebcfb5cad5bc993991c2aabd4
Malcolm Hendrix <melanna_camacho@Melannas-MacBook-Pro.local>
2022-04-11 19:03:50 -0700

    final commit on server.js

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const routes = require('./routes')
 const db = require('./config/connection');
 
 const app = express();
-const PORT: 3001;
-
+const PORT = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(routes);
 
-
+app.listen(PORT, () => {
+    db.once('open', () => {
+        console.log('API server running on port ${PORT}!');
+    });
+});
