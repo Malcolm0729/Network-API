d63417dc2fce542fbcb59ec2a4899d188d0f54b6
Malcolm Hendrix <melanna_camacho@Melannas-MacBook-Pro.local>
2022-04-20 18:06:24 -0700

    final commit

---
+++ routes/api/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const friendRoutes = require('./friendRoutes');
+const userRoutes = require('./userRoutes');
+const reactionRoutes = require('./reactionRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+//uses the api routes with these url queries
+router.use('/users', userRoutes);
+router.use('/users, friendRoutes');
+router.use('/thoughts, thoughtRoutes');
+router.use('/thoughts, reactionRoutes');
+
+module.exports = router;
