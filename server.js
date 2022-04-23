a1877c456818d13e2d052c90c007276a0426b771
Malcolm Hendrix <melanna_camacho@Melannas-MacBook-Pro.local>
2022-04-23 10:37:25 -0700

    making changes

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const routes = require('./routes')
+const routes = require('./routes');
 const db = require('./config/connection');
 
-const app = express();
 const PORT = 3001;
+const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(routes);
 
+db.once('open', () => {
 app.listen(PORT, () => {
-    db.once('open', () => {
         console.log('API server running on port ${PORT}!');
     });
 });
